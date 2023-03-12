7085c8d0da05659eeb7f5b6f5f45b09546a29931
Carlos Figueroa <carfig7186@students.ecpi.edu>
2023-03-11 21:56:56 -0500

    javascript started

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".navbar-menu");
+const subject = document.querySelector(".task-title");
+const message = document.querySelector(".task-body");
+const subjectInput = document.getElementById("subject");
+const messageInput = document.getElementById("text-body");
+const note = document.querySelector(".notes");
 
+// hamburger menu function
 menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
+});
+
+subject.addEventListener("submit", (e) => {
+  const taskTitle = subject.value;
+  const taskMessage = message.value;
 });
